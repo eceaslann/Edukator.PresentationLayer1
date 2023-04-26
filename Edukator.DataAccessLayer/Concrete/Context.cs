7ebed1c1ce7916755506c095bfc6ecbef2f55b6b
Ece <Ece@DESKTOP-RCI54VP>
2023-04-26 11:16:27 +0300

    Proje dosyası ekle.

---
+++ Edukator.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using Edukator.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-RCI54VP; initial catalog=DbEdukator; integrated security=true;");
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        
+
+    }
+}
